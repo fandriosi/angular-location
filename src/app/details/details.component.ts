80314ef63f87f73b55195f21d1e63e27743dbeb4
D99G <fabio.andriosi.SPASSU@petrobras.com.br>
2024-07-30 23:25:17 -0300

    first commit

---
+++ src/app/details/details.component.ts
@@ -0,0 +1,35 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housing-location';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-details',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: `./details.component.html`,
+  styleUrls: ['./details.component.css']
+})
+export class DetailsComponent {
+  router: ActivatedRoute = inject(ActivatedRoute);
+  housingLocationId = 0;
+  housingSerive = inject(HousingService);
+  housingLocation : HousingLocation|undefined;
+
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl('')
+  });
+  constructor(){
+    const housingLocationId = Number(this.router.snapshot.params['id'])
+    this.housingLocation = this.housingSerive.getHousingLocationById(housingLocationId);
+  }
+
+  submitApplication(){
+    this.housingSerive.submitApplication(this.applyForm.value.firstName??'', this.applyForm.value.lastName??'',
+      this.applyForm.value.email??'');
+  }
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housing-location';
+import { HousingService } from '../housing.service';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, HousingLocationComponent],
+  templateUrl: `./home.component.html`,
+  styleUrls: ['./home.component.css'],  
+})
+export class HomeComponent {
+  housingLocactionList: HousingLocation[]=[]
+  haousingService: HousingService = inject(HousingService)
+
+  constructor(){
+    this.housingLocactionList = this.haousingService.getAllHousingLocation();
+  }
+}
